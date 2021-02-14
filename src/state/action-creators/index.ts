9e973cf47b57807a8bd9f6db2b09c24e41cdf4d3
aeonincode <khashayar.sasannia@hotmail.com>
2021-02-14 18:38:22 +0100

    Adding Request Logic

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -9,7 +9,22 @@ const searchRepositories = (term: string) => {
         });
 
         try {
+            const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', {
+                // params get our query string into URL
+                params: {
+                    text: term
+                }
+            })
             
+            const names = data.objects.map((result: any) => {
+                return result.package.name;
+            });
+
+            dispatch({
+                type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+                payload: names
+            })
+        
         } catch (err) {
             dispatch({
                 type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
