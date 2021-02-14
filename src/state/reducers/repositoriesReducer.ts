cc92f7d30166a2b67c34d875de3b4174466c8062
aeonincode <khashayar.sasannia@hotmail.com>
2021-02-14 13:07:24 +0100

    Applying Action Interface

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -18,11 +18,19 @@ interface SearchRepositoriesErrorAction {
     payload: string;
 }
 
-const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+const reducer = (
+    state: RepositoriesState, 
+    action: 
+        | SearchRepositoriesAction 
+        | SearchRepositoriesSuccessAction 
+        | SearchRepositoriesErrorAction 
+):     RepositoriesState => {
+
     switch (action.type) {
         case 'search_repositories':
             return { loading: true, error: null, data: [] };
         case 'search_repositories_success':
+            // 100% certain that 'action' is SearchRepositoriesSuccessAction
             return { loading: false, error: null, data: action.payload }
         case 'search_repositories_error':
             return { loading: false, error: action.payload, data: [] };
