a161c4e3630a3f180f7f090b4e5c2eae55471bb6
aeonincode <khashayar.sasannia@hotmail.com>
2021-02-18 20:24:04 +0100

    Selecting State,, Awkward Typings Around React-Redux, Creating a Typed Selector

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 import { useActions} from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
+  // const state = useSelector((state: any) => state.repositories);
+  const { data, error, loading } = useTypedSelector((state) => state.repositories);
+  // console.log(state);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
