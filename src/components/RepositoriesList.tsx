31a7b76080ed415bb7d1e577bca864d7f1c3a65a
aeonincode <khashayar.sasannia@hotmail.com>
2021-02-19 02:37:04 +0100

    consuming Store State and finisg the app, end section 4

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -21,8 +21,16 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && 
+        data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
 
 export default RepositoriesList;
+
+// If error is null, then we are not going to show h3 {error && <h3>{error}</h3>}
+// If loading is true then print out h3 {loading && <h3>Loading...</h3>}
+// If there is no error, loading, printout our list of library or packages names {!error && !loading && data}
